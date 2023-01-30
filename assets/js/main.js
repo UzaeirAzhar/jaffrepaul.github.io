e4f4650738c805cab715d0124ddf2b0177ba2bf9
Uzaeir <paf_uzair@yahoo.com>
2023-01-30 13:56:30 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function ($) {
   $('.level-bar-inner').css('width', '60%')
   $(window).on('load', function () {
     $('.level-bar-inner').each(function () {
-      let itemWidth = $(this).attr('level')
+      let itemWidth = $(this).attr('data-level')
       console.log(itemWidth)
       //$('.level-bar-inner').css('width', itemWidth)
       $(this).animate(
